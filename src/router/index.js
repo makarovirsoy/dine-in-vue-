3b572e77d254427640788d8717f6d9c49632bae1
yosri gam <ygm@spacesquad.de>
2022-05-29 22:48:51 +0200

    frontoffice

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/home',
+            name: 'home',
+            component: () => import('../components/Customer/home.vue')
+        },
+        {
             path: '/dashboard',
             name: 'dashboard',
             component: () => import('../views/Dashboard.vue')
